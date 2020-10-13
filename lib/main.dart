ceeea4b4c0b878f0ab4b98b4f612e40bdde7ec37
FlutLab.io <root@vmi348945.contaboserver.net>
2020-10-13 20:31:11 +0200

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+// snap 5
+void main() => runApp(MyApp());
+
+// snap 4 - base state
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application
+  @override
+  Widget build(BuildContext context) {
+    //
+    return MaterialApp(
+      // Application name
+      title: 'Flutter Hello World',
+      // Application theme data, you can set the colors for the application as
+      // you want
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+      ),
+
+      // A widget which will be started on application startup
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
